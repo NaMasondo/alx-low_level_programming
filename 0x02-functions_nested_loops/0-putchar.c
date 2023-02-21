96d8632eaa561e7ed9001da8490bcf927229a394
NaMasondo <masondop96@gmail.com>
2023-02-21 11:36:02 -0800

    README.md and main.h files

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints _putchar and new line.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char c = '_putchar';
+
+	putchar("c\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+0-putchar.c, writes a programme that prints _putchar, followed by bew line.
