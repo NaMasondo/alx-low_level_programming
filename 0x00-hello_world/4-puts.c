be4801fccc3209db2fc423cdf80a7bd779079e1d
NaMasondo <masondop96@gmail.com>
2023-04-22 07:26:31 -0700

    4.Hello,puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description - prints a string
+ * Return: Always success (0)
+ */
+int main(void)
+{
+	char Str1[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(Str1);
+
+	return (0);
+}
