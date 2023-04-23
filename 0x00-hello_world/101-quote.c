a331e1096c1dde16ca096cf66f360b2d35abc89c
NaMasondo <masondop96@gmail.com>
2023-04-22 17:01:38 -0700

    8. UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - "prints a stringfollowed by a new line, to standard error."
+ *
+ * Return: Always 1.
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\"");
+	fprintf(stderr, " - Dora Korpar, 2015-10-19\n");
+
+	return (1);
+}
