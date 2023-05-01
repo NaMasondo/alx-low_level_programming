a3e9c4be3ab1170ea46ecde2f4b85147ac60ad3f
NaMasondo <masondop96@gmail.com>
2023-05-01 10:54:20 -0700

    0. isupper

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _islower - "checks for lowercase character"
+ *
+ * Return: 1 If "c" is lowercase and 0 otherwise.
+ */
+
+int _islower(int c)
+{
+	char c = 97;
+
+	if (c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	return (0);
+}
